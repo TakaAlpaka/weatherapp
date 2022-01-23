700710233819d8a9e2b7b01f71426cb99cc7cbd9
TakaAlpaka <strojny.magda@gmail.com>
2022-01-23 16:36:17 +0100

    text-align data

--- index.js
+++ index.js
@@ -112,7 +112,7 @@ function displayForecast(response) {
             forecastDay.weather[0].icon
           }@2x.png"
           alt=""
-          width="42"
+          width="60"
         /> </p>
             </div>
             <div class="col-4">
